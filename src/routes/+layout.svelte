337a587e976dcaa216ebba8a50c76133f5b18331
Patrik Pessi <patrik.pessi@gmail.com>
2023-07-02 19:19:17 +0300

    Add loading spinner for page loads

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
 	import '../app.pcss';
+
+	let loading = false;
+
+	beforeNavigate(() => {
+		loading = true;
+	});
+
+	afterNavigate(() => {
+		loading = false;
+	});
 </script>
 
-<slot />
+{#if loading}
+	<div class="m-auto animate-spin border-zinc-800 border-[0.75rem] border-t-blue-500 rounded-full w-24 h-24" />
+{:else}
+	<slot />
+{/if}
